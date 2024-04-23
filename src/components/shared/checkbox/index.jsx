2437b9f75ee17d25281d2fb4f246997fef94ead3
Alekos Dordas <dordasal@gmail.com>
2024-04-23 23:12:01 +0300

    Styles Checkbox component

--- src/components/shared/checkbox/index.jsx
+++ src/components/shared/checkbox/index.jsx
@@ -1,11 +1,25 @@
-import { forwardRef } from "react"
+import { forwardRef, useCallback, useState } from "react"
+import checkmark from "../../../assets/images/check.svg"
 import "./checkbox.scss"
 
-const Checkbox = forwardRef(({ label, id }, ref) => (
-  <span className="Checkbox">
-    <label htmlFor={id}>{label}</label>
-    <input type="checkbox" id={id} ref={ref} />
-  </span>
-))
+const Checkbox = forwardRef(({ label, id }, ref) => {
+  const [isChecked, setIsChecked] = useState(ref?.current?.checked || false)
+
+  const changeHandler = useCallback(e => {
+    setIsChecked(e.target.checked)
+  }, [])
+
+  return (
+    <span className={`Checkbox ${isChecked ? "Checkbox--checked" : ""}`}>
+      <label htmlFor={id}>
+        <span>{label}</span>
+        <span className="Checkbox__icon">
+          {isChecked && <img src={checkmark} alt="" />}
+        </span>
+      </label>
+      <input type="checkbox" id={id} ref={ref} onChange={changeHandler} />
+    </span>
+  )
+})
 
 export default Checkbox
